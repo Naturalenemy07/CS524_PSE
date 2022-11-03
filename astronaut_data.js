cff4866b50e6d2c40adbef8d97307be27fb2e71b
John Caruthers <john.d.caruthers@gmail.com>
2022-11-03 06:28:57 -0400

    Update astronaut_data.js

--- astronaut_data.js
+++ astronaut_data.js
@@ -1,10 +1,34 @@
 // creates a user
 
-class infoUser() {
-    //functions to create, aquire, modify user information
-    
-}
+class infoUser {
+   //functions to create, aquire, modify user information
+   //initialize class
+    constructor(username, password, email, scores) {
+        this.username = username; //string
+        this.password = password; //should we encrypt this before storing on users device?
+        this.email = email; //string
+        this.scores = scores; //tuple [0,0,0], how will scores be represented?
+    }
 
-class dataBase() {
-    // interact with the users local storage
+    //store information on local machine
+    storeInfo() {
+        localStorage.setItem("username",this.username)
+        localStorage.setItem("password",this.password)
+        localStorage.setItem("email",this.email)
+        localStorage.setItem("scores", this.scores)
+        
+        return 0
+    }
+
+    boolean
+    checkCredentials(inpUsername,inpPassword) {
+        if (inpUsername == localStorage.getItem("username")) {
+            if (inpPassword == localStorage.getItem("password")) {
+                return true
+            }
+        }
+        else {
+            return false
+        }
+    }
 }
