c603873d8ddbf22c422bd6f0a6df6e6b929d0257
John Caruthers <john.d.caruthers@gmail.com>
2022-11-13 14:09:47 -0500

    Update mathDefenseScript.js

--- mathDefenseScript.js
+++ mathDefenseScript.js
@@ -1,1 +1,9 @@
+//base skeleton of canvas
+var c = document.getElementById("gamecanvas");
+var ctx = c.getContext("2d"); // takes canvas and lets us draw, can be 3d
 
+//drawing commands for areas
+ctx.rect(0,00,400,450); //playable area
+ctx.rect(0,450,250,150); //equation area
+ctx.rect(250,450,150,150); // button area
+ctx.stroke(); //draws the rectangle
