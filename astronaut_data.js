fb2686028ed69eba9541dbf7316010cfd2074f27
John Caruthers <john.d.caruthers@gmail.com>
2022-11-20 15:08:51 -0500

    update

--- astronaut_data.js
+++ astronaut_data.js
@@ -12,32 +12,27 @@ class infoUser {
 
     //store information on local machine
     storeInfo() {
-        localStorage.setItem("username",this.username)
-        localStorage.setItem("password",this.password)
-        localStorage.setItem("email",this.email)
-        localStorage.setItem("scores", this.scores)
-        
-        return 0
-    }
+        localStorage.setItem("username",this.username);
+        localStorage.setItem("password",this.password);
+        localStorage.setItem("email",this.email);
+        localStorage.setItem("scores", this.scores);
+}
+}
 
-    //returns true if credentials match, otherwise returns false
-    checkCredentials(inpUsername,inpPassword) {
-        if (inpUsername == localStorage.getItem("username")) {
-            if (inpPassword == localStorage.getItem("password")) {
-                return true
-            }
+//returns true if credentials match, otherwise returns false
+function checkCredentials(inpUsername,inpPassword) {
+    if (inpUsername == localStorage.getItem("username")) {
+        if (inpPassword == localStorage.getItem("password")) {
+            return true;
         }
-        else {
-            return false
-        }
+    }
+    else {
+        return false;
     }
 }
 
-// test to verfiy javscript file was being called
-// document.write("Hello, I am from the Astronaut data JavaScript file");
-
-// function to create a new users, will be called from html file of the login screen
 function createNewUser(name, password, email) {
     const user = new infoUser(name,password,email,[0,0,0]);
+    document.writeln("say hello");
     user.storeInfo();
 }
