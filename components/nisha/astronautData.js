d3e0a657a8df3f5c3d00b3afe7f6f50f60bf267f
Noonliiess <118703581+Noonliiess@users.noreply.github.com>
2022-12-12 17:25:43 -0500

    Add files via upload

---
+++ components/nisha/astronautData.js
@@ -0,0 +1,42 @@
+
+// creates a user
+
+class infoUser {
+    //functions to create, aquire, modify user information
+    //initialize class
+    constructor(username, password, email, scores) {
+        this.username = username; //string
+        this.password = password; //should we encrypt this before storing on users device?
+        this.email = email; //string
+        this.scores = scores; //tuple [0,0,0], how will scores be represented?
+    }
+
+    //store information on local machine
+    storeInfo() {
+        localStorage.setItem("username",this.username);
+        localStorage.setItem("password",this.password);
+        localStorage.setItem("email",this.email);
+        localStorage.setItem("scores", this.scores);
+    }
+}
+
+//returns true if credentials match, otherwise returns false
+function checkCredentials(inpUsername,inpPassword) {
+    if (inpUsername == localStorage.getItem("username")) {
+        if (inpPassword == localStorage.getItem("password")) {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+function createNewUser(name, password, email) {
+    const user = new infoUser(name,password,email,[0,0,0]);
+    sendEmail(email);
+    // document.writeln("<h1>Email sent successfully!</h1>");
+    document.write('<h1 style="text-align: center;">Thank you!</h1>');
+    document.write('<h3 style="text-align: center;">You have successfully signed up.</h3>');
+    user.storeInfo();
+}
